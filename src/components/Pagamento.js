19c966a0630210cc3652c9f0cdc59c59d7b53b54
Edson Dota <edsondota@gmail.com>
2018-11-21 19:31:17 -0200

    Adicionar Rodapé

--- src/components/Pagamento.js
+++ src/components/Pagamento.js
@@ -7,6 +7,25 @@ class Pagamento extends Component {
       <div className="pagamento row mt-3">
         <div className="col-md-6 offset-md-3">
           <Header />
+          <div className="row mr-2 h-100">
+            <div className="col p-3 bg-light">
+              <div className="row conteudo">
+                <div className="col bg-transparent"></div>
+              </div>
+              <div className="row rodape">
+                <div className="col">
+                  <a href="#">
+                    <small>Retornar à Loja Demo</small>
+                  </a>
+                </div>
+                <div className="col text-right">
+                  <a href="#">
+                    <small>Ajuda</small>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
